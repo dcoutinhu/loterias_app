f87763d29463b20d55078a56686be9f29f3a2cfc
dcoutinhu <dcoutinhu@Ubuntu-Dev-Machine.myguest.virtualbox.org>
2024-01-11 22:09:29 -0300

    First commit for the loterias_app project

---
+++ app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""This app is created to check Brazilian lottery games"""
+
+__author__ = "Diego Coutinho"
+__date__ = "01/11/2024"
+__version__ = "1.0.0"
+__maintainer__ = "Diego Coutinho"
+__email__ = "diegoengcontrole@gmail.com"
+__status__ = "Development"
